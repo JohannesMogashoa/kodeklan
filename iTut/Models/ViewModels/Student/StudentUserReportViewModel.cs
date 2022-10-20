c43c4e37ffafa53df1d9cebaac3082a750faecbe
Teboho Gift Selane <100524455+Xblack17@users.noreply.github.com>
2022-10-20 19:18:12 +0200

    Student (#10)
    * Commit
    
    * commit 2.0
    
    * commit
    
    * Commit
    
    * Commit
    
    Co-authored-by: Teboho Gift Selane <s220848459@mandela.ac.za>
    Co-authored-by: Johannes Mogashoa <66737801+JohannesMogashoa@users.noreply.github.com>

---
+++ iTut/Models/ViewModels/Student/StudentUserReportViewModel.cs
@@ -0,0 +1,12 @@
+﻿
+using iTut.Models.Users;
+using System.Collections.Generic;
+
+namespace iTut.Models.ViewModels.Student
+{
+    public class StudentUserReportViewModel
+    {
+        public StudentUser Student { get; set; }
+       
+    }
+}
