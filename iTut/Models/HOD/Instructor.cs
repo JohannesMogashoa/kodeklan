04a9c40a28d48d05ce913cd3791f95f307fcbcaf
Johannes Mogashoa <jomogashoa1993@gmail.com>
2022-10-21 19:51:44 +0200

    Squashed commit of the following:
    commit 6de7d58f6f612c224214f06caf4d76690f068108
    Author: Johannes Mogashoa <jomogashoa1993@gmail.com>
    Date:   Thu Oct 20 19:12:47 2022 +0200
    
        Delete ApplicationDbContextModelSnapshot.cs
    
    commit 6033d16f570ec986df486f18a86f20765ecf86ab
    Author: Benevolence Netshiungani <s213464268@mandela.ac.za>
    Date:   Thu Oct 20 15:43:18 2022 +0200
    
        HOD STAFF

---
+++ iTut/Models/HOD/Instructor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using System;
+using iTut.Models.Users;
+
+namespace iTut.Models.HOD
+{
+    public class Instructor
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        [StringLength(50)]
+        public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Hire Date")]
+        public DateTime HireDate { get; set; }
+        public int? UserID { get; set; }
+        public virtual CoordinatorUser CoordinatorUser { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get { return LastName + ", " + FirstMidName; }
+        }
+
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
+        public OfficeAssignment OfficeAssignment { get; set; }
+    }
+}
